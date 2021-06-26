78edba6eeae23ac17dfd76f1dc65dd4b4ae7283d
ceny <senymama228@gmail.com>
2021-06-26 09:56:06 +0300

    upgrade import, add save to file
    обновлённый import, добавленно сохранение в файл

--- csv to dict.py
+++ csv to dict.py
@@ -1,14 +1,14 @@
-from tkinter.filedialog import *
-from tkinter import *
+import tkinter.filedialog as fd
+import tkinter as tk
 file = str
 dic = {}
 mas = []
 
 
 def my_write(els):
-    win = Tk()
+    win = tk.Tk()
     win.withdraw()
-    file_name = asksaveasfilename(filetypes=(("TXT files", "*.txt"),
+    file_name = fd.asksaveasfilename(filetypes=(("TXT files", "*.txt"),
                                              ("Chemical elements files", "*.els;*.el"),
                                              ("All files", "*.*")))
     f = open(file_name, 'w', encoding='UTF8')
@@ -18,10 +18,10 @@ def my_write(els):
 
 
 def my_read():
-    win = Tk()
+    win = tk.Tk()
     win.withdraw()
     global file
-    file_name = askopenfilename()
+    file_name = fd.askopenfilename()
     f = open(file_name, 'r', encoding='UTF8')
     file = f.read()
     f.close()
@@ -37,4 +37,4 @@ for i in file.split('\n'):
     my_dict(i)
     mas.append(dic)
 print(mas)
-
+my_write(mas)
